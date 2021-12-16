187b1c8c17cb480a748d6729f91548061b12c238
Martins Barinskis <martins@barinskis.me>
2021-12-16 17:55:54 +0200

    add loading indicator for results

--- src/ResultsPage.tsx
+++ src/ResultsPage.tsx
@@ -8,10 +8,16 @@ interface ResultsPageProps {
 
 export const ResultsPage = ({responses}: ResultsPageProps) => {
     const {userName, quizId} = useSettings();
-    const {loading, error, data: results} = useResults(quizId!, responses);
+    const {loading, error, data: results} = useResults(quizId!, responses); //quizId should always be defined here
     return (
         <div>
             <h1>{`Thanks ${userName}!`}</h1>
+            {loading && (
+                <>
+                    <p>Loading results of your quiz, please wait.</p>
+                    <progress />
+                </>
+            )}
             {results && <p>{`You responded correctly to ${results.correct} of ${results.total} questions.`}</p>}
         </div>
     );
